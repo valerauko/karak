d885b18b289c96fadc15438633981005f8a83c30
Unknown <vale@valerauko.net>
2018-11-25 20:34:06 +0900

    Add bold tests

--- test/karak/transformers_test.clj
+++ test/karak/transformers_test.clj
@@ -16,4 +16,13 @@
     (is (= "hoge <em>fuga piyo</em>"
            (naive-flattener (italic "hoge *fuga piyo*"))))
     (is (= "<em>hoge fuga piyo</em>"
-           (naive-flattener (italic "*hoge fuga piyo*"))))))
+           (naive-flattener (italic "*hoge fuga piyo*")))))
+  (testing "Two asterisks means bold"
+    (is (= "hoge <strong>fuga</strong> piyo"
+           (naive-flattener (bold "hoge **fuga** piyo"))))
+    (is (= "<strong>hoge fuga</strong> piyo"
+           (naive-flattener (bold "**hoge fuga** piyo"))))
+    (is (= "hoge <strong>fuga piyo</strong>"
+           (naive-flattener (bold "hoge **fuga piyo**"))))
+    (is (= "<strong>hoge fuga piyo</strong>"
+           (naive-flattener (bold "**hoge fuga piyo**"))))))
